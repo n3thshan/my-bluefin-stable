1efd9a293a5a6681af70d03c78d102b19f32a27a
n3thshan <n3thshan@github.com>
2026-05-24 11:02:07 +0530

    Repo name-change associated commits

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-# dnf5 install -y tmux 
+# dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -19,10 +19,12 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+# Brave
 dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-dnf5 install -y brave-browser 
+dnf5 install -y brave-browser
 dnf5 config-manager disable brave-browser
 
+# Bitwarden
 curl -L "https://bitwarden.com/download/?app=desktop&platform=linux&variant=rpm" -o /tmp/bitwarden.rpm
 dnf install -y /tmp/bitwarden.rpm
 rm /tmp/bitwarden.rpm
